2b87f91bb10e08f2dace7d3ae4a5aeedb739f013
Manasa <manasa@Manasas-MacBook-Air.local>
2026-06-30 10:51:29 +0530

    Started Factory Method Pattern

---
+++ 01-Design-Patterns/FactoryMethodPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Document
+{
+    public virtual void Open()
+    {
+        Console.WriteLine("Opening a document");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
